3c36e7b4896c440ca0018f5e3653b2d0792ae989
Araujo144p <kircaua5661@gmail.com>
2025-11-28 17:03:59 -0300

    chore: instalar EF Core e ferramentas do dotnet-ef

--- ToDoList/obj/Debug/net10.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net10.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b6324c2177c7abe601c87c3a4b551280ec6adb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+605a19db98f44aa90a961acb5d7e8961daf252b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
