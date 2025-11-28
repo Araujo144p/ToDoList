605a19db98f44aa90a961acb5d7e8961daf252b1
Araujo144p <kircaua5661@gmail.com>
2025-11-28 16:54:22 -0300

    feat: TarefaModel Criada

---
+++ ToDoList/Models/TarefaModel.cs
@@ -0,0 +1,19 @@
+namespace ToDoList.Models;
+
+public class TarefaModel
+{
+    public TarefaModel(string nome, string descricao, bool concluida)
+    {
+        Id = Guid.NewGuid();
+        DataCriação = DateTime.Now;
+        Nome = nome;
+        Descricao = descricao;
+        Concluida = concluida;
+    }
+
+    public Guid Id { get; init; }
+    public DateTime DataCriação {get; init;}
+    public string Nome { get; set; } = String.Empty;
+    public string Descricao { get; set; } = String.Empty;
+    public bool Concluida {get; set;} = false;
+}
--- ToDoList/obj/Debug/net10.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net10.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b6324c2177c7abe601c87c3a4b551280ec6adb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
