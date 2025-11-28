ab0a600ccb37dc2b3a31cf801826875e65cf8e33
Araujo144p <kircaua5661@gmail.com>
2025-11-28 19:50:24 -0300

    feat: Criado validacoes em DTO

--- ToDoList/DTOs/TarefaCreateDto.cs
+++ ToDoList/DTOs/TarefaCreateDto.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDoList.DTOs
 {
     public class TarefaCreateDto
     {
+        [Required]
+        [MinLength(3)]
         public string Nome { get; set; } = String.Empty;
         public string Descrição { get; set; } = String.Empty;
     }
